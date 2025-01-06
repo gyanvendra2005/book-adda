3677169202c6fdfd6d6f519a5122844d087c5f5d
Gyanvendra Singh <gyanvendras2004@gmail.com>
2025-01-06 15:58:59 +0530

    sell book book page ready

--- src/app/(app)/sell-book/page.tsx
+++ src/app/(app)/sell-book/page.tsx
@@ -131,205 +131,199 @@ export default function BookSellingPage() {
 
   return (
     <>
-      {/* Back Button and Page Header */}
-      <div className="items-center mt-10 mx-4">
-        <button
-          className="text-3xl text-orange-500 hover:text-orange-700 dark:text-white flex items-center ml-8  m-2"
-          onClick={() => history.back()}
-        >
-          <IoMdArrowRoundBack className=""/>
-        </button>
-        <h1 className="ml-8 font-bold text-xl text-gray-800 dark:text-white">
-          SELL-BOOKS
-        </h1>
+   <div className="items-center mt-10 mx-4">
+  <button
+    className="text-3xl text-orange-500 hover:text-orange-700 dark:text-white flex items-center ml-8 m-2"
+    onClick={() => history.back()}
+  >
+    <IoMdArrowRoundBack />
+  </button>
+  <h1 className="ml-8 font-bold text-xl text-gray-800 dark:text-white">
+    SELL-BOOKS
+  </h1>
+</div>
+
+<div className="flex flex-col md:flex-row flex-wrap w-full md:w-3/4 h-auto justify-center border shadow-lg mx-auto mt-10 lg:mt-20 rounded-xl p-4">
+
+  {/* Information Section */}
+  <div className="md:w-2/5 w-full md:h-auto h-1/3 mt-8 md:mt-24 bg-gradient-to-br from-orange-500 to-red-800 dark:from-zinc-900 dark:to-zinc-900 text-white rounded-md font-medium shadow-lg p-6 md:p-10">
+    <h2 className="text-2xl md:text-3xl font-bold mb-6">Ready to Sell?</h2>
+    <p className="text-sm md:text-base mb-4">
+      Provide the book details. Share a good description and pictures to make it appealing to buyers.
+    </p>
+    <h3 className="text-xl md:text-2xl font-semibold mb-4">Info About Condition</h3>
+    <ul className="list-disc ml-6 space-y-2">
+      <li><strong>Like New:</strong> The book looks almost brand new with minimal signs of wear.</li>
+      <li><strong>Good:</strong> Great condition with minor signs of use (e.g., slight page yellowing).</li>
+      <li><strong>Acceptable:</strong> Noticeable wear but readable (e.g., dog-eared pages).</li>
+    </ul>
+  </div>
+
+  {/* Form Section */}
+  <div className="w-full lg:w-1/2 bg-white dark:bg-black rounded-md p-6 md:p-8 m-4">
+    <form className="my-8 space-y-6" onSubmit={handleSubmit} encType="multipart/form-data">
+      {/* Book Name and Author */}
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
+        <LabelInputContainer>
+          <Label htmlFor="bookname">Book Name*</Label>
+          <Input
+            id="bookname"
+            placeholder="Book Title"
+            type="text"
+            value={bookName}
+            onChange={(e) => setBookName(e.target.value)}
+            className="bg-gray-200 p-2 rounded-md"
+          />
+        </LabelInputContainer>
+
+        <LabelInputContainer>
+          <Label htmlFor="authorname">Author (optional)</Label>
+          <Input
+            id="authorname"
+            placeholder="Author Name"
+            type="text"
+            value={authorName}
+            onChange={(e) => setAuthorName(e.target.value)}
+            className="bg-gray-200 p-2 rounded-md"
+          />
+        </LabelInputContainer>
       </div>
-   
-    <div className="flex flex-col lg:flex-row flex-wrap w-full lg:w-3/4 h-auto justify-center border shadow-lg mx-auto mt-10 lg:mt-20 rounded-xl">
 
-   
-    <div className="w-full lg:w-2/5 h-2/3 mt-24 bg-gradient-to-br from-orange-500 to-red-800 dark:from-zinc-900 dark:to-zinc-900 text-white rounded-md font-medium shadow-lg p-10 m-4">
-      <h2 className="text-4xl font-bold mb-8">Ready to Sell?</h2>
-      <p className="text-lg mb-4">
-        Provide the book details. Share a good description and pictures to make it appealing to buyers.
-      </p>
-      <h3 className="text-2xl font-semibold mb-4">Info About Condition</h3>
-      <ul className="list-disc ml-6 space-y-2">
-        <li><strong>Like New:</strong> The book looks almost brand new with minimal signs of wear.</li>
-        <li><strong>Good:</strong> Great condition with minor signs of use (e.g., slight page yellowing).</li>
-        <li><strong>Acceptable:</strong> Noticeable wear but readable (e.g., dog-eared pages).</li>
-      </ul>
-    </div>
-      {/* Form */}
-         <div className="w-full lg:w-1/2 bg-white dark:bg-black rounded-md p-8 m-4">
-        <form className="my-8 space-y-6  " onSubmit={handleSubmit} encType="multipart/form-data">
-          {/* Book Name and Author */}
-          <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
+      {/* Edition and Price */}
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
+        <LabelInputContainer>
+          <Label htmlFor="edition">Edition (optional)</Label>
+          <Input
+            id="edition"
+            placeholder="Edition"
+            type="text"
+            value={edition}
+            onChange={(e) => setEdition(e.target.value)}
+            className="bg-gray-200 p-2 rounded-md"
+          />
+        </LabelInputContainer>
 
-            {/* book name */}
-            <LabelInputContainer>
-              <Label htmlFor="bookname">Book Name*</Label>
-              <Input
-                id="bookname"
-                placeholder="Book Title"
-                type="text"
-                value={bookName}
-                onChange={(e) => setBookName(e.target.value)}
-                className="bg-gray-200"
-              />
-            </LabelInputContainer>
-
-            {/* author */}
-            <LabelInputContainer>
-              <Label htmlFor="authorname">Author(optional)</Label>
-              <Input
-                id="authorname"
-                placeholder="Author Name"
-                type="text"
-                value={authorName}
-                onChange={(e) => setAuthorName(e.target.value)}
-                className="bg-gray-200"
-              />
-             </LabelInputContainer>
-          </div>
-
-
-          <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
-            {/* estiton */}
-          <LabelInputContainer>
-           <Label htmlFor="authorname">Edition(optional)</Label>
-             <Input
-                id="authorname"
-                placeholder="Author Name"
-                type="text"
-                value={edition}
-                onChange={(e) => setEdition(e.target.value)}
-                className="bg-gray-200"
-              />
-            </LabelInputContainer>
-
-          {/* Price */}
-          <LabelInputContainer>
-            <Label htmlFor="price">Price (₹)*</Label>
-            <Input
-              id="price"
-              placeholder="₹250"
-              type="number"
-              value={price}
-              onChange={(e) => setPrice(e.target.value)}
-              className="bg-gray-200"
-            />
-          </LabelInputContainer>
-          </div>
-
-          {/* Category */}
-          <LabelInputContainer>
-            <Label htmlFor="category">Category*</Label>
-            <select
-              id="category"
-              value={category}
-              onChange={(e) => setCategory(e.target.value)}
-              className="px-4 py-3 rounded-md border border-neutral-300 dark:border-neutral-700 dark:bg-neutral-800 bg-gray-200 focus:ring-2 focus:ring-pink-500 focus:outline-none"
-            >
-              <option value="">Select a Category</option>
-              {bookCategories.map((category, index) => (
-                <option key={index} value={category}>{category}</option>
-              ))}
-            </select>
-            </LabelInputContainer>
-
-          {/* Description */}
-           <LabelInputContainer>
-            <Label htmlFor="description">Description(optional)</Label>
-            <textarea
-              id="description"
-              placeholder="Description"
-              value={description}
-              onChange={(e) => {setDescription(e.target.value); setWords(e.target.value.length)} }
-              className="bg-gray-200 rounded-md p-2 h-40 w-full"
-              maxLength={250}
-            />
-            <p className="text-right text-sm mt-2">{words}/250</p>
-           </LabelInputContainer>
-
-          {/* Condition */}
-          <LabelInputContainer>
-          <Label>Select Book Condition*</Label>
-            <select
-              id="condition"
-              value={condition}
-              onChange={(e) => setCondition(e.target.value)}
-              className="px-4 py-3 rounded-md border border-neutral-300 dark:border-neutral-700 dark:bg-neutral-800 focus:ring-2 bg-gray-200 focus:ring-pink-500 focus:outline-none"
-            >
-              <option value="">Select Condition</option>
-              {bookConditions.map((condition, index) => (
-                <option key={index} value={condition}>{condition}</option>
-              ))}
-            </select>
-            </LabelInputContainer>
-
-          <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
-          {/* City */}
-          <LabelInputContainer>
-            <Label htmlFor="city">City*</Label>
-            <Input
-              id="city"
-              placeholder="City Name"
-              type="text"
-              value={city}
-              onChange={(e) => setCity(e.target.value.toLowerCase())}
-              className="bg-gray-200"
-            />
-            <p className={`${isCityFound ? 'text-green-500' : 'text-red-500'}`}>{message}</p>
-            </LabelInputContainer>
-
-          {/* Images */}
-          <LabelInputContainer>
-            <Label htmlFor="files">Images*</Label>
-            <Input
-              id="files"
-              type="file"
-              name="images"
-              multiple
-              accept="image/*"
-              onChange={(e) => setFiles(Array.from(e.target.files || []))}
-              className="bg-gray-200"
-            />
-          </LabelInputContainer>
-          </div>
-
-          {/* Terms and Conditions */}
-          <div className="flex items-center space-x-2">
-            <input
-              type="checkbox"
-              id="terms"
-              checked={termsAccepted}
-              onChange={() => setTermsAccepted(!termsAccepted)}
-              className="w-4 h-4 text-blue-600 rounded"
-            />
-            <label htmlFor="terms" className="text-sm text-neutral-600 dark:text-neutral-300">
-              I agree to the <Link href="/terms-conditions" className="text-blue-600 underline">Terms and Conditions</Link>
-            </label>
-          </div>
-
-          {/* Submit Button */}
-          <button
-          className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-          type="submit" 
-        >
-          {isSubmitting ? (
-           <div className="flex items-center justify-center">
-              <Loader2 className="h-4 w-4 animate-spin mr-2" />
-               <span>Please wait...</span>
-                </div>
-            ) : (
-                 'Submit'
-                )}
-
-          <BottomGradient />
-        </button>
-        </form>
-        </div>
+        <LabelInputContainer>
+          <Label htmlFor="price">Price (₹)*</Label>
+          <Input
+            id="price"
+            placeholder="₹250"
+            type="number"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            className="bg-gray-200 p-2 rounded-md"
+          />
+        </LabelInputContainer>
       </div>
+
+      {/* Category */}
+      <LabelInputContainer>
+        <Label htmlFor="category">Category*</Label>
+        <select
+          id="category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          className="px-4 py-3 rounded-md border border-neutral-300 dark:border-neutral-700 dark:bg-neutral-800 bg-gray-200 focus:ring-2 focus:ring-pink-500 focus:outline-none"
+        >
+          <option value="">Select a Category</option>
+          {bookCategories.map((category, index) => (
+            <option key={index} value={category}>{category}</option>
+          ))}
+        </select>
+      </LabelInputContainer>
+
+      {/* Description */}
+      <LabelInputContainer>
+        <Label htmlFor="description">Description (optional)</Label>
+        <textarea
+          id="description"
+          placeholder="Description"
+          value={description}
+          onChange={(e) => { setDescription(e.target.value); setWords(e.target.value.length) }}
+          className="bg-gray-200 rounded-md p-2 h-40 w-full"
+          maxLength={250}
+        />
+        <p className="text-right text-sm mt-2">{words}/250</p>
+      </LabelInputContainer>
+
+      {/* Condition */}
+      <LabelInputContainer>
+        <Label>Select Book Condition*</Label>
+        <select
+          id="condition"
+          value={condition}
+          onChange={(e) => setCondition(e.target.value)}
+          className="px-4 py-3 rounded-md border border-neutral-300 dark:border-neutral-700 dark:bg-neutral-800 focus:ring-2 bg-gray-200 focus:ring-pink-500 focus:outline-none"
+        >
+          <option value="">Select Condition</option>
+          {bookConditions.map((condition, index) => (
+            <option key={index} value={condition}>{condition}</option>
+          ))}
+        </select>
+      </LabelInputContainer>
+
+      {/* City */}
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
+        <LabelInputContainer>
+          <Label htmlFor="city">City*</Label>
+          <Input
+            id="city"
+            placeholder="City Name"
+            type="text"
+            value={city}
+            onChange={(e) => setCity(e.target.value.toLowerCase())}
+            className="bg-gray-200 p-2 rounded-md"
+          />
+          <p className={`${isCityFound ? 'text-green-500' : 'text-red-500'}`}>{message}</p>
+        </LabelInputContainer>
+
+        <LabelInputContainer>
+          <Label htmlFor="files">Images*</Label>
+          <Input
+            id="files"
+            type="file"
+            name="images"
+            multiple
+            accept="image/*"
+            onChange={(e) => setFiles(Array.from(e.target.files || []))}
+            className="bg-gray-200 p-2 rounded-md"
+          />
+        </LabelInputContainer>
+      </div>
+
+      {/* Terms and Conditions */}
+      <div className="flex items-center space-x-2">
+        <input
+          type="checkbox"
+          id="terms"
+          checked={termsAccepted}
+          onChange={() => setTermsAccepted(!termsAccepted)}
+          className="w-4 h-4 text-blue-600 rounded"
+        />
+        <label htmlFor="terms" className="text-sm text-neutral-600 dark:text-neutral-300">
+          I agree to the <Link href="/terms-conditions" className="text-blue-600 underline">Terms and Conditions</Link>
+        </label>
+      </div>
+
+      {/* Submit Button */}
+      <button
+        className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+        type="submit"
+      >
+        {isSubmitting ? (
+          <div className="flex items-center justify-center">
+            <Loader2 className="h-4 w-4 animate-spin mr-2" />
+            <span>Please wait...</span>
+          </div>
+        ) : (
+          'Submit'
+        )}
+        <BottomGradient />
+      </button>
+    </form>
+  </div>
+</div>
+
     </>
   );
 }
